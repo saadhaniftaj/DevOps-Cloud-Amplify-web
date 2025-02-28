2ee3aafff8363452e67202c94a2024d960bea093
sameer-ahmed92167 <sameer92167@gmail.com>
2025-03-01 04:59:22 +0500

    displaying user profile 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState, useEffect } from "react";
 import {
   Button,
@@ -15,7 +16,7 @@ import outputs from "../amplify_outputs.json";
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
-
+/*
 Amplify.configure(outputs);
 const client = generateClient({
   authMode: "userPool",
@@ -75,4 +76,4 @@ export default function App() {
       <Button onClick={signOut}>Sign Out</Button>
     </Flex>
   );
-}
+}*/
